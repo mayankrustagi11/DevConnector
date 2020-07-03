e017e71d3c14715be751cdac69d5394071c1ad1c
Mayank Rustagi <mayankrustagi11@gmail.com>
2020-07-03 23:51:14 +0530

    Fixed isAuthenticated in Auth Reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -36,7 +36,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         token: null,
-        isAuthenticated: true,
+        isAuthenticated: false,
         loading: false,
       };
     default:
