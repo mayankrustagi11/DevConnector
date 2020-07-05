3a5004ad9b4c9c0a589c2ce47e432adaef36ae59
Mayank Rustagi <mayankrustagi11@gmail.com>
2020-07-05 17:32:25 +0530

    Profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -27,9 +27,10 @@ export const getCurrentProfile = () => async (dispatch) => {
 export const getProfiles = () => async (dispatch) => {
   dispatch({ type: CLEAR_PROFILE });
   try {
-    const res = await axios.get('/api/profiles');
+    const res = await axios.get('/api/profile');
     dispatch({ type: GET_PROFILES, payload: res.data });
   } catch (err) {
+    console.log(err);
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
